d1b85c6b725d1ec60365b5d94450b303aa8980e1
Heros <heros.goodoy@gmail.com>
2023-10-24 00:30:20 -0300

    projeto completo

---
+++ Episodio.cs
@@ -0,0 +1,21 @@
+﻿class Episodio
+{
+    private List<string> convidados = new List<string>();
+
+    public Episodio(string titulo, int duracao, int ordem)
+    {
+        Titulo = titulo;
+        Duracao = duracao;
+        Ordem = ordem;
+    }
+
+    public int Duracao { get; }
+    public int Ordem { get; }
+    public string Resumo => $"{Ordem}. {Titulo} ({Duracao} min) - {string.Join(", ", convidados)}";
+    public string Titulo { get; set; }
+
+    public void AdicionarConvidados (string convidado)
+    {
+        convidados.Add (convidado);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Episodio ep1 = new("Técnicas de facilitação", 45, 2);
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("Marcelo");
+
+Episodio ep2 = new("Técnicas de aprendizado", 67, 1);
+ep2.AdicionarConvidados("Fernando");
+ep2.AdicionarConvidados("Marcos");
+ep2.AdicionarConvidados("Flavia");
+
+Podcast podcast = new("Daniel", "Podcast especial");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.ExibirDetalhes();
+
+
